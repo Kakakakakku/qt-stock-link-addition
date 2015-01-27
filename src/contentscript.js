5b34ba6bd8bb1db0c36a9312e409d1ed66297f4c
kakakakakku <y.yoshida22@gmail.com>
2015-01-27 19:13:24 +0900

    一般ユーザでも使えるように DOM のセレクタを変更した

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,4 +1,4 @@
 var user_path = $(".dropdown-menu:last > li > a:first").attr('href');
 var stock_path = user_path + '/stock';
-var base = $(".hidden-xs:eq(4)");
-base.after('<li class="hidden-xs"><a href="' + stock_path + '">ストック一覧</a></li>');
+var base = $(".globalNotifications");
+base.before('<li class="hidden-xs"><a href="' + stock_path + '">ストック一覧</a></li>');
